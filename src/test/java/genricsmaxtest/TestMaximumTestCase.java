041e64ee3461c38955eb1f95c9ac05d242ebfa33
beni2412 <hardaman97@gmail.com>
2020-10-09 01:59:46 +0530

    [Hardaman] Added: Find mx of three integers and checking the test cases

--- src/test/java/genricsmaxtest/TestMaximumTestCase.java
+++ src/test/java/genricsmaxtest/TestMaximumTestCase.java
@@ -4,11 +4,42 @@
 package genricsmaxtest;
 
 import org.junit.Test;
+
+import com.capg.testmax.TestMaximum;
+
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
+
 public class TestMaximumTestCase {
-    @Test public void testSomeLibraryMethod() {
-        Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
-    }
+	@Test
+	public void givenFirst_Greater_ShouldReturnFirstInteger() {
+		TestMaximum test = new TestMaximum();
+		Integer result = test.maxOfThree(100, 90, 80);
+		boolean finalResult=false; 
+		if(result == 100) {
+		finalResult = true;
+		} 
+		Assert.assertEquals(true, finalResult);;
+	}
+	@Test
+	public void givenSecond_Greater_ShouldReturnSecondInteger() {
+		TestMaximum test = new TestMaximum();
+		Integer result = test.maxOfThree(90, 100, 80);
+		boolean finalResult=false; 
+		if(result == 100) {
+		finalResult = true;
+		} 
+		Assert.assertEquals(true, finalResult);;
+	}
+	@Test
+	public void givenThird_Greater_ShouldReturnThirdInteger() {
+		TestMaximum test = new TestMaximum();
+		Integer result = test.maxOfThree(90, 80, 100);
+		boolean finalResult=false; 
+		if(result == 100) {
+		finalResult = true;
+		} 
+		Assert.assertEquals(true, finalResult);;
+	}
 }
