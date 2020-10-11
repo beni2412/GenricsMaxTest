67350f90e9903ad857f5d1452b1820c7432b6b02
beni2412 <hardaman97@gmail.com>
2020-10-12 03:32:46 +0530

    [Hardaman] Added: Runing test cases using collections

--- src/main/java/com/capg/testmax/TestMaximum.java
+++ src/main/java/com/capg/testmax/TestMaximum.java
@@ -1,26 +1,22 @@
 package com.capg.testmax;
 
-public class TestMaximum <E extends Comparable<E>> {
-	E i1,i2,i3;
-	
-	public TestMaximum(E i1,E i2,E i3) {
-		this.i1 = i1;
-		this.i2 = i2;
-		this.i3 = i3;
-	}
-	public static <E extends Comparable<E>> E maxOfThree(E a, E b, E c){
-		E max = a;
-		if(b.compareTo(max) > 0) {
-			max = b;
-		}
-		if(c.compareTo(max) > 0) {
-			max = c;
-		}
-		return max;
-	}
-	public <E extends Comparable<E>> E maxOfThree() {
-		return (E) maxOfThree(i1,i2,i3);
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TestMaximum<E extends Comparable<E>>  {
+	ArrayList<E> array = new ArrayList<E>();
+	public ArrayList<E> addVal(E a, E b, E c, E d){
+		array.add(a);
+		array.add(b);
+		array.add(c);
+		array.add(d);
 		
+		return array;
+	}
+	public static <E extends Comparable> E maximumInArrayList(ArrayList<E> a)
+	{
+		Collections.sort(a);
+    	return a.get(a.size()-1);
 	}
 	public static void main(String[] args) {
 		System.out.println("Test Maximum");
--- src/test/java/genricsmaxtest/TestMaximumTestCase.java
+++ src/test/java/genricsmaxtest/TestMaximumTestCase.java
@@ -9,13 +9,17 @@ import com.capg.testmax.TestMaximum;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Assert;
 
 public class TestMaximumTestCase{
 	@Test
 	public void givenFirst_Greater_ShouldReturnFirstInteger() {
-		TestMaximum test = new TestMaximum<Integer>(100,90,80);
-		Integer result = (Integer) test.maxOfThree();
+		TestMaximum test = new TestMaximum();
+		ArrayList<Integer> array = new ArrayList<>();
+    	array = test.addVal(100,90,80,70);
+		Integer result = (Integer) test.maximumInArrayList(array);
 		boolean finalResult=false; 
 		if(result == 100) {
 		finalResult = true;
@@ -24,8 +28,10 @@ public class TestMaximumTestCase{
 	}
 	@Test
 	public void givenSecond_Greater_ShouldReturnSecondInteger() {
-		TestMaximum test = new TestMaximum<Integer>(90,100,80);
-		Integer result = (Integer) test.maxOfThree();
+		TestMaximum test = new TestMaximum();
+		ArrayList<Integer> array = new ArrayList<>();
+    	array = test.addVal(90,100,80,70);
+		Integer result = (Integer) test.maximumInArrayList(array);
 		boolean finalResult=false; 
 		if(result == 100) {
 		finalResult = true;
@@ -34,8 +40,10 @@ public class TestMaximumTestCase{
 	}
 	@Test
 	public void givenThird_Greater_ShouldReturnThirdInteger() {
-		TestMaximum test = new TestMaximum<Integer>(80,90,100);
-		Integer result = (Integer) test.maxOfThree();
+		TestMaximum test = new TestMaximum();
+		ArrayList<Integer> array = new ArrayList<>();
+    	array = test.addVal(90,80,100,70);
+		Integer result = (Integer) test.maximumInArrayList(array);
 		boolean finalResult=false; 
 		if(result == 100) {
 		finalResult = true;
@@ -44,8 +52,10 @@ public class TestMaximumTestCase{
 	}
 	@Test
 	public void givenFirst_Greater_ShouldReturnFirstFloat() {
-		TestMaximum test = new TestMaximum<Float>(100f,90f,80f);
-		Float result = (Float) test.maxOfThree();
+		TestMaximum test = new TestMaximum();
+		ArrayList<Float> array = new ArrayList<>();
+    	array = test.addVal(100f,90f,80f,70f);
+		Float result = (Float) test.maximumInArrayList(array);
 		boolean finalResult=false; 
 		if(result == 100f) {
 		finalResult = true;
@@ -54,8 +64,10 @@ public class TestMaximumTestCase{
 	}
 	@Test
 	public void givenSecond_Greater_ShouldReturnSecondFloat() {
-		TestMaximum test = new TestMaximum<Float>(90f,100f,80f);
-		Float result = (Float) test.maxOfThree();
+		TestMaximum test = new TestMaximum();
+		ArrayList<Float> array = new ArrayList<>();
+    	array = test.addVal(90f,100f,80f,70f);
+		Float result = (Float) test.maximumInArrayList(array);
 		boolean finalResult=false; 
 		if(result == 100f) {
 		finalResult = true;
@@ -64,8 +76,10 @@ public class TestMaximumTestCase{
 	}
 	@Test
 	public void givenThird_Greater_ShouldReturnThirdFloat() {
-		TestMaximum test = new TestMaximum<Float>(80f,90f,100f);
-		Float result = (Float) test.maxOfThree();
+		TestMaximum test = new TestMaximum();
+		ArrayList<Float> array = new ArrayList<>();
+    	array = test.addVal(90f,80f,100f,70f);
+		Float result = (Float) test.maximumInArrayList(array);
 		boolean finalResult=false; 
 		if(result == 100f) {
 		finalResult = true;
@@ -74,8 +88,10 @@ public class TestMaximumTestCase{
 	}
 	@Test
 	public void givenFirst_Greater_ShouldReturnFirstString() {
-		TestMaximum test = new TestMaximum<String>("peach", "banana", "apple");
-		String result = (String) test.maxOfThree();
+		TestMaximum test = new TestMaximum();
+		ArrayList<String> array = new ArrayList<>();
+    	array = test.addVal("peach","banana","apple","cat");
+		String result = (String) test.maximumInArrayList(array);
 		boolean finalResult=false; 
 		if(result.equalsIgnoreCase("peach")) {
 		finalResult = true;
@@ -84,8 +100,10 @@ public class TestMaximumTestCase{
 	}
 	@Test
 	public void givenSecond_Greater_ShouldReturnSecondString() {
-		TestMaximum test = new TestMaximum<String>("banana", "peach", "apple");
-		String result = (String) test.maxOfThree();
+		TestMaximum test = new TestMaximum();
+		ArrayList<String> array = new ArrayList<>();
+    	array = test.addVal("banana","peach","apple","cat");
+		String result = (String) test.maximumInArrayList(array);
 		boolean finalResult=false; 
 		if(result.equalsIgnoreCase("peach")) {
 		finalResult = true;
@@ -94,8 +112,10 @@ public class TestMaximumTestCase{
 	}
 	@Test
 	public void givenThird_Greater_ShouldReturnThirdString() {
-		TestMaximum test = new TestMaximum<String>("banana", "apple", "peach");
-		String result = (String) test.maxOfThree();
+		TestMaximum test = new TestMaximum();
+		ArrayList<String> array = new ArrayList<>();
+    	array = test.addVal("banana","apple","peach","cat");
+		String result = (String) test.maximumInArrayList(array);
 		boolean finalResult=false; 
 		if(result.equalsIgnoreCase("peach")) {
 		finalResult = true;
