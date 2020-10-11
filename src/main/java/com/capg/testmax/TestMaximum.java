f7ec0d3d72488344eef790d67721b4c23c888891
beni2412 <hardaman97@gmail.com>
2020-10-12 02:35:09 +0530

    [Hardaman] Refactored: To one genric method to find maximum

--- src/main/java/com/capg/testmax/TestMaximum.java
+++ src/main/java/com/capg/testmax/TestMaximum.java
@@ -1,30 +1,9 @@
 package com.capg.testmax;
 
 public class TestMaximum {
-	public int maxOfThree(Integer a, Integer b, Integer c) {
-		Integer max = a;
-		if (b.compareTo(max) > 0) {
-			max = b;
-		}
-		if (c.compareTo(max) > 0) {
-			max = c;
-		}
-		return max;
-	}
-	
-	public float maxOfThree(Float a, Float b, Float c) {
-		Float max = a;
-		if(b.compareTo(max)>0) {
-			max=b;
-		}
-		if(c.compareTo(max)>0) {
-			max=c;
-		}
-		return max;
-	}
-	
-	public String maxOfThree(String a, String b, String c) {
-		String max = a;
+	public static <E extends Comparable> E maxOfThree(E a,E b,E c)
+	{
+		E max = a;
 		if(b.compareTo(max)>0) {
 			max=b;
 		}
