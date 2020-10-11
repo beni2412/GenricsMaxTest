acac0db56b1e5c4f16f7922ed7edac5eb77ce349
beni2412 <hardaman97@gmail.com>
2020-10-12 03:39:20 +0530

    [Hardaman] Refactored: Using genric methods to extend max and print the max

--- src/main/java/com/capg/testmax/TestMaximum.java
+++ src/main/java/com/capg/testmax/TestMaximum.java
@@ -3,24 +3,39 @@ package com.capg.testmax;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class TestMaximum<E extends Comparable<E>>  {
+public class TestMaximum<E extends Comparable<E>> {
+	E a, b, c, d;
 	ArrayList<E> array = new ArrayList<E>();
-	public ArrayList<E> addVal(E a, E b, E c, E d){
+
+	public TestMaximum(E a, E b, E c, E d) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = d;
+	}
+
+	public ArrayList<E> addVal() {
 		array.add(a);
 		array.add(b);
 		array.add(c);
 		array.add(d);
-		
+
 		return array;
 	}
-	public static <E extends Comparable> E maximumInArrayList(ArrayList<E> a)
-	{
+
+	public static <E extends Comparable> E maximumInArrayList(ArrayList<E> a) {
 		Collections.sort(a);
-    	return a.get(a.size()-1);
+		printMaximum(a);
+		return a.get(a.size() - 1);
 	}
+
+	public static <E> void printMaximum(ArrayList<E> a) {
+		System.out.println(a.get(a.size() - 1));
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Test Maximum");
-		
+
 	}
 
 }
--- src/test/java/genricsmaxtest/TestMaximumTestCase.java
+++ src/test/java/genricsmaxtest/TestMaximumTestCase.java
@@ -16,9 +16,9 @@ import org.junit.Assert;
 public class TestMaximumTestCase{
 	@Test
 	public void givenFirst_Greater_ShouldReturnFirstInteger() {
-		TestMaximum test = new TestMaximum();
+		TestMaximum test = new TestMaximum<Integer>(100,90,80,70);
 		ArrayList<Integer> array = new ArrayList<>();
-    	array = test.addVal(100,90,80,70);
+    	array = test.addVal();
 		Integer result = (Integer) test.maximumInArrayList(array);
 		boolean finalResult=false; 
 		if(result == 100) {
@@ -28,9 +28,9 @@ public class TestMaximumTestCase{
 	}
 	@Test
 	public void givenSecond_Greater_ShouldReturnSecondInteger() {
-		TestMaximum test = new TestMaximum();
+		TestMaximum test = new TestMaximum<Integer>(90,100,80,70);
 		ArrayList<Integer> array = new ArrayList<>();
-    	array = test.addVal(90,100,80,70);
+		array = test.addVal();
 		Integer result = (Integer) test.maximumInArrayList(array);
 		boolean finalResult=false; 
 		if(result == 100) {
@@ -40,9 +40,9 @@ public class TestMaximumTestCase{
 	}
 	@Test
 	public void givenThird_Greater_ShouldReturnThirdInteger() {
-		TestMaximum test = new TestMaximum();
+		TestMaximum test = new TestMaximum<Integer>(90,80,100,70);
 		ArrayList<Integer> array = new ArrayList<>();
-    	array = test.addVal(90,80,100,70);
+    	array = test.addVal();
 		Integer result = (Integer) test.maximumInArrayList(array);
 		boolean finalResult=false; 
 		if(result == 100) {
@@ -52,9 +52,9 @@ public class TestMaximumTestCase{
 	}
 	@Test
 	public void givenFirst_Greater_ShouldReturnFirstFloat() {
-		TestMaximum test = new TestMaximum();
+		TestMaximum test = new TestMaximum<Float>(100f,90f,80f,70f);
 		ArrayList<Float> array = new ArrayList<>();
-    	array = test.addVal(100f,90f,80f,70f);
+    	array = test.addVal();
 		Float result = (Float) test.maximumInArrayList(array);
 		boolean finalResult=false; 
 		if(result == 100f) {
@@ -64,9 +64,9 @@ public class TestMaximumTestCase{
 	}
 	@Test
 	public void givenSecond_Greater_ShouldReturnSecondFloat() {
-		TestMaximum test = new TestMaximum();
+		TestMaximum test = new TestMaximum<Float>(90f,100f,80f,70f);
 		ArrayList<Float> array = new ArrayList<>();
-    	array = test.addVal(90f,100f,80f,70f);
+    	array = test.addVal();
 		Float result = (Float) test.maximumInArrayList(array);
 		boolean finalResult=false; 
 		if(result == 100f) {
@@ -76,9 +76,9 @@ public class TestMaximumTestCase{
 	}
 	@Test
 	public void givenThird_Greater_ShouldReturnThirdFloat() {
-		TestMaximum test = new TestMaximum();
+		TestMaximum test = new TestMaximum<Float>(90f,80f,100f,70f);
 		ArrayList<Float> array = new ArrayList<>();
-    	array = test.addVal(90f,80f,100f,70f);
+    	array = test.addVal();
 		Float result = (Float) test.maximumInArrayList(array);
 		boolean finalResult=false; 
 		if(result == 100f) {
@@ -88,9 +88,9 @@ public class TestMaximumTestCase{
 	}
 	@Test
 	public void givenFirst_Greater_ShouldReturnFirstString() {
-		TestMaximum test = new TestMaximum();
+		TestMaximum test = new TestMaximum<String>("peach","banana","apple","cat");
 		ArrayList<String> array = new ArrayList<>();
-    	array = test.addVal("peach","banana","apple","cat");
+    	array = test.addVal();
 		String result = (String) test.maximumInArrayList(array);
 		boolean finalResult=false; 
 		if(result.equalsIgnoreCase("peach")) {
@@ -100,9 +100,9 @@ public class TestMaximumTestCase{
 	}
 	@Test
 	public void givenSecond_Greater_ShouldReturnSecondString() {
-		TestMaximum test = new TestMaximum();
+		TestMaximum test = new TestMaximum<String>("banana","peach","apple","cat");
 		ArrayList<String> array = new ArrayList<>();
-    	array = test.addVal("banana","peach","apple","cat");
+    	array = test.addVal();
 		String result = (String) test.maximumInArrayList(array);
 		boolean finalResult=false; 
 		if(result.equalsIgnoreCase("peach")) {
@@ -112,9 +112,9 @@ public class TestMaximumTestCase{
 	}
 	@Test
 	public void givenThird_Greater_ShouldReturnThirdString() {
-		TestMaximum test = new TestMaximum();
+		TestMaximum test = new TestMaximum<String>("banana","apple","peach","cat");
 		ArrayList<String> array = new ArrayList<>();
-    	array = test.addVal("banana","apple","peach","cat");
+    	array = test.addVal();
 		String result = (String) test.maximumInArrayList(array);
 		boolean finalResult=false; 
 		if(result.equalsIgnoreCase("peach")) {
